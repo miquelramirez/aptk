813a816d7c41f191ffacf249c93f84bbbc22b87c
nirlipo <nirlipo@gmail.com>
2011-10-31 12:57:37 +0100

    fixed regression to be s' = s - add + prec

--- toolkit/src/planning/State.cxx
+++ toolkit/src/planning/State.cxx
@@ -39,7 +39,7 @@ State* State::regress_through( Action& a )
 	for ( unsigned k = 0; k < m_fluent_vec.size(); k++ )
 		if ( !a.asserts( m_fluent_vec[k] ) )
 			succ->set( m_fluent_vec[k] );
-	succ->set( a.del_vec() );
+	succ->set( a.prec_vec() );
 
 	return succ;
 }
